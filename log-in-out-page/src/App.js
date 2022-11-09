5add8832dd8370a8b8b6b790d7d632db4ba1b941
Chandan Kumar <krc6893@gmail.com>
2022-11-09 10:57:20 +0530

    log-in

---
+++ log-in-out-page/src/App.js
@@ -0,0 +1,20 @@
+
+import './components/styles.css';
+import { LoginPage } from './components/LoginPage';
+import { WelcomePage } from './components/WelcomePage';
+import { useState } from 'react';
+
+function App() {
+const [logIn,setLogin] = useState(false);
+const [id, setId] = useState("");
+
+  return (
+    <div className="App">
+      {logIn ? <WelcomePage   setLogin={setLogin} id={id} /> : <LoginPage  setLogin={setLogin} setId={setId} id={id} />}
+      {/* <LoginPage /> */}
+      {/* <WelcomePage /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ log-in-out-page/src/components/LoginPage.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+
+export function LoginPage({setLogin,setId,id}) {
+  
+  const [password, setPassword] = useState("");
+  const [idPwd, setIdPwd] = useState({ id, password });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(idPwd);
+
+    setIdPwd({ ...idPwd, id, password });
+    console.log(idPwd);
+
+    if (id === "" || password === "") {
+      setLogin(false);
+    }else{
+      setLogin(true);
+    }
+
+    // setLogin(true)
+    // setId("");
+    // setPassword("");
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="loginPage"
+          type="text"
+          placeholder="Enter UserId"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+
+        <input
+          className="loginPage"
+          type="text"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="login" type="submit">
+          Login
+        </button>
+      </form>
+
+      {/* {<WelcomePage idAndPassword={setIdPwd} />} */}
+    </>
+  );
+}
